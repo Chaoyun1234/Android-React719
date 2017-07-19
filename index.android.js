418156f2807e6051a959e2bc7b34d744bcade72c
Chaoyun1234 <dongchaoyun01@beyondsoft.com>
2017-07-19 02:47:20 -0700

    UpdateInstallId

--- index.android.js
+++ index.android.js
@@ -3,34 +3,81 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import MobileCenter from 'mobile-center';
+import Analytics from "mobile-center-analytics";
+import Crashes from "mobile-center-crashes";
+import Push from 'mobile-center-push';
 import React, { Component } from 'react';
 import {
+  AppState,
+  Alert,
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
+Analytics.trackEvent("My first event");
 export default class React719 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+                <Button
+                    title="crash"
+                    onPress={onCrash}
+                >
+                </Button>   
+                <Button
+                    title="click"
+                    onPress={onClick}
+                >
+                </Button>
+            </View>
+        );
+    }
+}
+//generateTestCrash
+function onCrash() {
+    throw new Error("This is a test JavaScript Crash");
+}
+ //generate install ID
+async function onClick() {
+    const installId = await MobileCenter.getInstallId();
+    Alert.alert(installId);
 }
 
+Push.setEventListener({
+    pushNotificationReceived: function (pushNotification) {
+        let message = pushNotification.message;
+        let title = pushNotification.title;
+
+        if (message === null || message === undefined) {
+            // Android messages received in the background don't include a message. On Android, that fact can be used to
+            // check if the message was received in the background or foreground. For iOS the message is always present.
+            title = "Android background";
+            message = "<empty>";
+        }
+
+        // Custom name/value pairs set in the Mobile Center web portal are in customProperties
+        if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+            message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+        }
+        if (AppState.currentState === 'active') {
+            Alert.alert(title, message);
+        }
+
+        else {
+            // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+            // In this case you'll want to save off the notification info and wait until the app is fully shown
+            // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+            // when the app is fully in the foreground.
+        }
+    }
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
